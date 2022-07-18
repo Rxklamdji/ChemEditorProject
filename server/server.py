1fa707f054704006a4e4dd8737c2a51346473e94
Romeo Klamadji <Romeo.Klamadji@fda.hhs.gov>
2022-07-18 08:58:46 -0500

    DeepDili Simple Version Integration!

--- server/server.py
+++ server/server.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request
 from flask import Request
 
+app = Flask(__name__)
+
+@app.route("/", methods=['POST', 'GET'])
+def home():
+    if request.method == 'POST':
+        search = ""
+        return redirect(url_for())
+    else:
+        return render_template('index.html')
+
+@app.route("/<src>")
+def search(src):
+    return ""
 
 
 
-
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
